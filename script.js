dd3c3effdf65075a233020704b431d964598363d
Jo Keeley <student@Souths-MBP-7.lan>
2023-03-15 14:04:34 -0500

    completed HW

---
+++ script.js
@@ -0,0 +1,31 @@
+// 1
+// const
+
+// 2
+let num = 20;
+console.log(num);
+
+// 3
+num = 15;
+console.log(num);
+
+// 4
+num = num + 7
+console.log(num);
+
+// 5
+num--
+console.log(num);
+
+// 6
+num = "This is not a number"
+console.log(num);
+
+// 7
+let blank;
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
