6f8c23056ffdd99c4670e232f50a503a0abb6113
Jo Keeley <student@Souths-MBP-7.lan>
2023-03-16 09:17:36 -0500

    bonus qs

--- script.js
+++ script.js
@@ -29,3 +29,19 @@ console.log(blank);
 
 // 9
 blank = null;
+console.log(blank);
+
+// BONUS
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
+
+// 2
+console.log(quarter, dime, nickel, penny);
+
+// 3
+let letter = "qwertyuiop"
+
+// 4
+let pSpot = "qwertyuiop".length;
+pSpot--;
+console.log(letters[pSpot]);
